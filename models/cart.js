0ea6e19449ba593a09564b3130673c38b32a3907
s-rashid <shahriar.8943@gmail.com>
2020-09-14 23:53:37 -0400

    Files

---
+++ models/cart.js
@@ -0,0 +1,69 @@
+
+module.exports = class Cart
+{
+    constructor(oldCart)
+    {
+        this.items = oldCart.items || {};
+        this.totalQty = oldCart.totalQty || 0;
+        this.totalPrice = oldCart.totalPrice || 0;
+        this.discountPrice = oldCart.discountPrice || 0;
+        this.userId = oldCart.userId || "";
+    }
+
+    add(item, id)
+    {
+        let storedItem = this.items[id];
+        if (!storedItem){
+            storedItem = this.items[id] = {item: item, qty: 0, price: 0};
+        }
+        storedItem.qty++;
+        storedItem.price = storedItem.item.price * storedItem.qty;
+        storedItem.price = parseFloat(storedItem.price.toFixed(2));
+        this.totalQty++;
+        this.totalPrice += storedItem.item.price;
+        this.totalPrice = parseFloat(this.totalPrice.toFixed(2));
+    }
+
+    decreaseQty(id)
+    {
+        this.items[id].qty--;
+        this.items[id].price -= this.items[id].item.price;
+        this.items[id].price = parseFloat(this.items[id].price.toFixed(2));
+        this.totalQty--;
+        this.totalPrice -= this.items[id].item.price       
+        this.totalPrice = parseFloat(this.totalPrice.toFixed(2));
+
+        if(this.items[id].qty <= 0) {
+            delete this.items[id];
+        }
+    }
+
+    increaseQty(id)
+    {
+        this.items[id].qty++;
+        this.items[id].price += this.items[id].item.price;
+        this.items[id].price = parseFloat(this.items[id].price.toFixed(2));
+        this.totalQty++;
+        this.totalPrice += this.items[id].item.price
+        this.totalPrice = parseFloat(this.totalPrice.toFixed(2));
+    }
+
+    generateArray()
+    {
+        let arr = [];
+        for (let id in this.items) {
+            arr.push(this.items[id])
+        }
+        return arr;
+    }
+
+    clearCart()
+    {
+        for (let id in this.items) {
+            let storedItem = this.items[id];
+            for (let index = storedItem.qty; index > 0; index--) {
+                this.decreaseQty(id)
+            }
+        }
+    }
+}
